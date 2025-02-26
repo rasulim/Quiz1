f2bdeac7b265f7f278a3676a509c1ba0e4abc367
rasulim <rasulim@code.edu.az>
2025-02-26 17:43:05 +0400

    Update mesele4.java

--- src/Q2_6_1array/mesele4.java
+++ src/Q2_6_1array/mesele4.java
@@ -3,4 +3,13 @@ package Q2_6_1array;
 // həmin massivdəki bütün ədədlərin kvadratlarını hesablayıb ekrana yazdırın.
 
 public class mesele4 {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5};
+        for (int i = 0; i < nums.length; i++) {
+            int kvadrat = nums[i] * nums[i];
+
+            System.out.println(nums[i] + " ədədinin kvadratı: " + kvadrat);
+        }
+    }
 }
