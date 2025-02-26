51125a91d223be3a71ae950a4a7f8ba1a7d16a1d
rasulim <rasulim@code.edu.az>
2025-02-26 17:49:44 +0400

    Update mesele1.java

--- src/Q2_6_4array/mesele1.java
+++ src/Q2_6_4array/mesele1.java
@@ -1,6 +1,12 @@
 package Q2_6_4array;
 // İstifadəçidən 6 ədəd daxil edin və bu ədədləri kiçikdən böyüyə doğru sıralayın.
 // (Bunu etmək üçün sort metodundan istifadə edə bilərsiniz.)
+import java.util.Arrays;
 
 public class mesele1 {
+    public static void main(String[] args) {
+        Integer[] a = {2,-1,3,4,0,8};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
