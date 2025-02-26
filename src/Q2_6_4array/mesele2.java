3f73d1249f486faa4302ad324009fcd99a8758b4
rasulim <rasulim@code.edu.az>
2025-02-26 17:50:18 +0400

    Update mesele2.java

--- src/Q2_6_4array/mesele2.java
+++ src/Q2_6_4array/mesele2.java
@@ -1,5 +1,12 @@
 package Q2_6_4array;
 //İstifadəçidən 8 ədəd daxil edin və həmin ədədləri böyükdən kiçiyə doğru sıralayın.
+import java.util.Arrays;
+import java.util.Collections;
 
 public class mesele2 {
+    public static void main(String[] args) {
+        Integer[] a = {2,-1,3,4,0,258,8,9};
+        Arrays.sort(a, Collections.reverseOrder());
+        System.out.println(Arrays.toString(a));
+    }
 }
