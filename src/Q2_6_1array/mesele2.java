6002e36a19a325dc66deca3a029dba09002e0c7e
rasulim <rasulim@code.edu.az>
2025-02-26 17:41:54 +0400

    Update mesele2.java

--- src/Q2_6_1array/mesele2.java
+++ src/Q2_6_1array/mesele2.java
@@ -1,5 +1,26 @@
 package Q2_6_1array;
 // İstifadəçidən 10 ədəd daxil edin və bu ədədlərin ən böyüyünü və ən kiçiyini tapın.
+import java.util.Scanner;
 
 public class mesele2 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("10 ədəd daxil edin: ");
+        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(" Ədəd " + (i+1) + ": ");
+            arr[i] = sc.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Ən böyüyük ədəd: " + max);
+        System.out.println("Ən kiçik ədəd: " + min);
+    }  
 }
