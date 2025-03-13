2103a541dec6bb151032ba4e2c6d07b53c4ea5fd
rasulim <rasulim@code.edu.az>
2025-03-13 06:19:29 +0400

    quiz 1 and 2

--- src/Q2_6_5array/mesele3.java
+++ src/Q2_6_5array/mesele3.java
@@ -1,5 +1,22 @@
 package Q2_6_5array;
 // Bir massiv yaradın və həmin massivdəki ədədləri ikiqat artırın. Məsələn, 1, 2, 3 massivini 2, 4, 6-ya çevirin.
 
+import java.util.Arrays;
+
 public class mesele3 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Əvvəlki massiv: " + Arrays.toString(arr));
+
+        doubleElements(arr);
+
+        System.out.println("İkiqat artırılmış massiv: " + Arrays.toString(arr));
+    }
+
+    public static void doubleElements(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] *= 2; // Hər bir elementi 2 ilə vururuq
+        }
+    }
 }
--- src/Q2_6_5array/mesele4.java
+++ src/Q2_6_5array/mesele4.java
@@ -2,5 +2,47 @@ package Q2_6_5array;
 // Bir massivdəki iki ədədin yerini dəyişin. İstifadəçidən massivdə iki index daxil edin və
 // həmin indexlərdəki ədədləri bir-birinə mübadilə edin.
 
+import java.util.Scanner;
+
 public class mesele4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Əvvəlki massiv:");
+        printArray(arr);
+
+        System.out.print("Birinci indexi daxil edin: ");
+        int index1 = scanner.nextInt();
+
+        System.out.print("İkinci indexi daxil edin: ");
+        int index2 = scanner.nextInt();
+
+        swapElements(arr, index1, index2);
+
+        System.out.println("Mübadilə olunmuş massiv:");
+        printArray(arr);
+
+        scanner.close();
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void swapElements(int[] arr, int index1, int index2) {
+
+        if (index1 < 0 || index1 >= arr.length || index2 < 0 || index2 >= arr.length) {
+            System.out.println("Daxil edilən indekslər səhvdir.");
+            return;
+        }
+
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
 }
